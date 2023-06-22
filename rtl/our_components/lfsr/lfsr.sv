253ef80a591725a5637d9030dd18310629834f88
Ishan Joshi <joshi.ishan23@gmail.com>
2023-06-21 22:53:56 -0700

    Added simulation for LFSR and annotated image

--- rtl/our_components/lfsr/lfsr.sv
+++ rtl/our_components/lfsr/lfsr.sv
@@ -4,16 +4,17 @@ module linear_feedback_shift_register_5_bit
 (
 	input logic clk,
 	input logic reset,
-	output logic [4:0] lfsr = 5'b00001; // Initial value
-	
+	output logic [4:0] lfsr = 5'b00001 // Initial value
 );
 
+	/* XOR Gate */
 	logic feedback;
 	assign feedback = lfsr[0] ^ lfsr[2];
 	
 	/* Sequential block */
 	always_ff @(posedge clk, posedge reset)
 	begin
+		/* Asynchronous Reset to initialize LFSR registers */
 		if (reset) lfsr <= 5'b00001;
 		else
 		begin
@@ -21,7 +22,7 @@ module linear_feedback_shift_register_5_bit
 			lfsr[2] <= lfsr[3];
 			lfsr[1] <= lfsr[2];
 			lfsr[0] <= lfsr[1];
-			reg4 <= feedback;
+			lfsr[4] <= feedback;
 		end
 	end
 	
